004d1c18aa15dd05161f2f63f6a3ede4a32ab4c3
Matt <mjruddy94@gmail.com>
2022-01-22 00:07:02 -0500

    💅 add picture hover

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,4 +1,4 @@
-import { VStack, Heading, Text, Image, Flex } from '@chakra-ui/react'
+import { VStack, Heading, Text, Image, Flex, Box } from '@chakra-ui/react'
 import WithSpeechBubbles from './Bubbles'
 
 export const Portfolio = () => {
@@ -11,7 +11,34 @@ export const Portfolio = () => {
         <Text fontSize='lg' color='gray.600' pb='10'>
           Referican is a trusted partner
         </Text>
-        <Image rounded='lg' src='/images/referican.jpeg' shadow='dark-lg' />
+        <Box
+          as='a'
+          href='https://referican.com'
+          target={'_blank'}
+          cursor={'pointer'}
+          _hover={{
+            div: {
+              opacity: 0.8,
+            },
+          }}
+          pos='relative'>
+          <Image rounded='lg' src='/images/referican.jpeg' shadow='dark-lg' />
+          <VStack
+            opacity={0}
+            transition={'opacity 0.6s ease'}
+            pos='absolute'
+            bottom={0}
+            h={'75%'}
+            w='100%'
+            pb='6'
+            justify='flex-end'
+            roundedBottom={'lg'}
+            bg='linear-gradient(0deg, rgba(0,0,0,1) 20%, rgba(0,0,0,0) 100%)'>
+            <Text as='h3' color={'white'} fontSize='xl'>
+              View Referican
+            </Text>
+          </VStack>
+        </Box>
         <WithSpeechBubbles />
       </VStack>
     </Flex>
