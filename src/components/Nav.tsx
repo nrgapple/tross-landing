fd5a7d914beab9e84d5411d46e7dca3930860dca
AG <champtoday@gmail.com>
2022-01-21 19:15:59 -0500

    💅 logo woah

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,29 @@
-import { Flex, Heading, HStack, Text } from '@chakra-ui/react'
+import {
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  Image,
+  Box,
+  Icon,
+  keyframes,
+} from '@chakra-ui/react'
+import { hexToCSSFilter } from '../lib/hex-css'
+import theme from '../theme'
+import { TrossLogo } from './TrossLogo'
 
 export const Nav = () => {
+  const color1 = theme.colors.brand[600]
+  const color2 = theme.colors.brand[100]
+  const color3 = theme.colors.brand[400]
+  const logoColor = keyframes`
+    0%, 100%{
+      color: ${color1};
+    }
+    50% {
+      color: ${color2};
+    }
+  `
   return (
     <Flex w='full' justify='center'>
       <HStack
@@ -11,23 +34,32 @@ export const Nav = () => {
         align='center'
         justify='space-between'
         w='full'>
-        <Heading color='brand.600'>
-          <Text
-            as={'span'}
-            position={'relative'}
-            _after={{
-              content: "''",
-              width: 'full',
-              height: '40%',
-              position: 'absolute',
-              bottom: 1,
-              left: 0,
-              bg: 'brand.100',
-              zIndex: -1,
-            }}>
-            Tross
-          </Text>
-        </Heading>
+        <HStack>
+          <TrossLogo
+            color={color1}
+            animation={`${logoColor} infinite 5s linear`}
+            w='100px'
+            h='100px'
+            viewBox='0 0 500 500'
+          />
+          <Heading color='brand.600'>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: '40%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'brand.100',
+                zIndex: -1,
+              }}>
+              Tross
+            </Text>
+          </Heading>
+        </HStack>
       </HStack>
     </Flex>
   )
---
+++ src/components/TrossLogo.tsx
@@ -0,0 +1,150 @@
+import { createIcon, Icon } from '@chakra-ui/react'
+
+export const TrossLogo = createIcon({
+  displayName: 'TrossLogo',
+  viewBox: '0 0 500 500',
+  path: (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={500}
+      height={500}
+      xmlSpace='preserve'>
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='translate(232.54 184.53)'
+        d='M-217.108 48.986.216-46.959l83.108-22.973 133.784 124.54h-25.676L83.324-46.959.216-24.662l-217.324 94.594z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='matrix(1 0 0 1.04 241.27 211.22)'
+        d='M-8.73-50V56.081H8.73V-56.08z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='matrix(1 0 0 1.12 306.54 197.23)'
+        d='M-8.73-50V56.081H8.73V-56.08z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='translate(244.03 272.7)'
+        d='m-228.595 14.19 457.19-44.595v17.567l-457.19 43.243z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='translate(270.25 214.08)'
+        d='M-16.052-29.555v69.932l17.616-1.4v-57.53l14.488-4.879v-16.945l-32.104 9.414v2.72z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='matrix(1 0 0 .84 270.25 328.84)'
+        d='M-16.052-29.555v69.932H1.564v-58.93l14.488-4.879v-16.945l-32.104 3.668v8.466z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='matrix(1 0 0 .98 241.27 324.08)'
+        d='m-8.73-37.741 17.46-1.495v78.472H-8.73z'
+      />
+      <path
+        style={{
+          stroke: 'currentColor',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: 'currentColor',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        vectorEffect='non-scaling-stroke'
+        transform='matrix(1 0 0 1.03 306.54 321.06)'
+        d='M-8.73-38.776 8.73-40.27v80.542H-8.73z'
+      />
+    </svg>
+  ),
+})
